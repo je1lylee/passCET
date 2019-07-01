38a1e0248042298258c877ed388e078ce79b66d0
907033509 <907033509@qq.com>
2019-07-01 10:01:03 +0800

    跳转

--- app/src/main/java/top/linxixiangxin/passcet/MainActivity.java
+++ app/src/main/java/top/linxixiangxin/passcet/MainActivity.java
@@ -1,13 +1,27 @@
 package top.linxixiangxin.passcet;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button asd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        asd=findViewById(R.id.as);
+        asd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
